57139b334285bc6509052e3204142fe9d5c35fae
Stefan Ciprian <stefanciprian444@gmail.com>
2024-09-02 11:01:02 +0300

    refactor: initial project structure and styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#0a97b4",
+        "primary-content": "#c1f2fc",
+        "primary-dark": "#076e84",
+        "primary-light": "#0dc0e4",
+
+        background: "#F5F5F5",
+        foreground: "#FBFBFB",
+        border: "#dde1e2",
+
+        copy: "#232829",
+        "copy-light": "#5e6b6e",
+        "copy-lighter": "#849295",
+
+        success: "#0ab40a",
+        warning: "#b4b40a",
+        error: "#b40a0a",
+
+        "success-content": "#c1fcc1",
+        "warning-content": "#000000",
+        "error-content": "#fcc1c1",
       },
     },
   },
